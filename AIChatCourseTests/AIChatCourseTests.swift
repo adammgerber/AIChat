1a1c841b78a654ff348faadb219af3cbc734a07c
adammgerber <agerber7777@gmail.com>
2025-09-09 12:08:32 +0300

    Initial Commit

---
+++ AIChatCourseTests/AIChatCourseTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatCourseTests.swift
+//  AIChatCourseTests
+//
+//  Created by Adam Gerber on 09/09/2025.
+//
+
+import Testing
+@testable import AIChatCourse
+
+struct AIChatCourseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
