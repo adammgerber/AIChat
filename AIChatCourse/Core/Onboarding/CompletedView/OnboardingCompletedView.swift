cc4c89e737f93eb79b31f5f7a07341fb85fe8420
adammgerber <agerber7777@gmail.com>
2025-09-12 13:09:44 +0300

    [Feature] Completed Onboarding

--- AIChatCourse/Core/Onboarding/CompletedView/OnboardingCompletedView.swift
+++ AIChatCourse/Core/Onboarding/CompletedView/OnboardingCompletedView.swift
@@ -10,28 +10,62 @@ import SwiftUI
 struct OnboardingCompletedView: View {
     
     @Environment(AppState.self) private var root
+    
+    @State private var isCompletingProfileSetup: Bool = false
+    var selectedColor: Color = .orange
+    
     var body: some View {
-        VStack {
-            Text("Onboarding Completed!")
-                .frame(maxHeight: .infinity)
+        VStack(alignment: .leading, spacing: 12) {
+            Text("Setup complete!")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundStyle(selectedColor)
             
-            Button {
-               onFinishedButtonPressed()
-            } label: {
-                Text("Finish")
-                    .callToActionButton()
-                
-            }
+            Text("We've set up your profile and you're ready to start chatting.")
+                .font(.title)
+                .fontWeight(.medium)
+                .foregroundStyle(.secondary)
         }
-        .padding(16)
+        .frame(maxHeight: .infinity)
+        .safeAreaInset(edge: .bottom, content: {
+            ctaButton
+        })
+        .padding(24)
+        .toolbar(.hidden, for: .navigationBar)
+    }
+    
+    private var ctaButton: some View {
+        Button {
+           onFinishedButtonPressed()
+        } label: {
+            ZStack {
+                if isCompletingProfileSetup {
+                    ProgressView()
+                        .tint(.white)
+                } else {
+                    Text("Finish")
+                        
+                }
+            }
+            .callToActionButton()
+        }
+        .disabled(isCompletingProfileSetup)
     }
     
     func onFinishedButtonPressed() {
-        // other logic to complete onboarding
-        root.updateViewState(showTabbarView: true)
+        isCompletingProfileSetup = true
+        Task {
+            try await Task.sleep(for: .seconds(3))
+            isCompletingProfileSetup = false
+            // try await saveUserProfile(color: selectedColor)
+            
+            // other logic to complete onboarding
+            root.updateViewState(showTabbarView: true)
+        }
+        
     }
 }
 #Preview {
-    OnboardingCompletedView()
+    OnboardingCompletedView(selectedColor: .mint)
         .environment(AppState())
 }
---
+++ AIChatCourse/Core/Onboarding/IntroView/OnboardingIntroView.swift
@@ -0,0 +1,49 @@
+//
+//  OnboardingIntroView.swift
+//  AIChatCourse
+//
+//  Created by Adam Gerber on 12/09/2025.
+//
+
+import SwiftUI
+
+struct OnboardingIntroView: View {
+    var body: some View {
+        VStack {
+            Group {
+                Text("Make your own ")
+                +
+                Text("avatars ")
+                    .foregroundStyle(.accent)
+                    .fontWeight(.semibold)
+                +
+                Text("and chat with them!\nHave ")
+                +
+                Text("real conversations ")
+                    .foregroundStyle(.accent)
+                    .fontWeight(.semibold)
+                +
+                Text("with AI generated responses.")
+            }
+            .baselineOffset(6)
+            .frame(maxHeight: .infinity)
+            .padding(24)
+            
+            NavigationLink {
+                OnboardingColorView()
+            } label: {
+                Text("Continue")
+                    .callToActionButton()
+            }
+        }
+        .padding(24)
+        .font(.title)
+        .toolbar(.hidden, for: .navigationBar)
+    }
+}
+
+#Preview {
+    NavigationStack {
+        OnboardingIntroView()
+    }
+}
