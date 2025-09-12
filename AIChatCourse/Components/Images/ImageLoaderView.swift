b99fab533a28110e9db9a407355d6d2cb6f39602
adammgerber <agerber7777@gmail.com>
2025-09-12 11:31:00 +0300

    [Feature] Add WelcomeView UI

---
+++ AIChatCourse/Components/Images/ImageLoaderView.swift
@@ -0,0 +1,28 @@
+//
+//  ImageLoaderView.swift
+//  AIChatCourse
+//
+//  Created by Adam Gerber on 12/09/2025.
+//
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+struct ImageLoaderView: View {
+    var urlString: String = Constants.randomImage
+    var resizingMode: ContentMode = .fill
+    
+    var body: some View {
+        Rectangle()
+            .opacity(0)
+            .overlay(
+                WebImage(url: URL(string: urlString))
+                    .resizable()
+                    .indicator(.activity)
+                    .aspectRatio(contentMode: resizingMode)
+                    .allowsHitTesting(false)
+            )
+            .clipped()
+    }
+    
+}
--- AIChatCourse/Core/Welcome/WelcomeView.swift
+++ AIChatCourse/Core/Welcome/WelcomeView.swift
@@ -8,21 +8,70 @@
 import SwiftUI
 
 struct WelcomeView: View {
+    
+    @State var imageName: String = Constants.randomImage
+    
     var body: some View {
         NavigationStack {
-            VStack {
-                Text("Welcome")
-                    .frame(maxHeight: .infinity)
+            VStack(spacing: 8) {
+                ImageLoaderView(urlString: imageName)
+                    .ignoresSafeArea()
                 
-                NavigationLink {
-                    OnboardingCompletedView()
-                } label: {
-                    Text("Get Started")
-                        .callToActionButton()
+                titleSection
+                .padding(.top, 24)
+               
+                ctaButtons
+                .padding(16)
+                
+                policyLinks
+            }
+            
+        }
+    }
+    
+    private var titleSection: some View {
+        VStack(spacing: 8) {
+            Text("AI Chat 🤙")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+            Text("YouTube @ SwiftfulThinking")
+                .font(.caption)
+                .foregroundStyle(.secondary)
+        }
+    }
+    
+    private var ctaButtons: some View {
+        VStack(spacing: 8) {
+            NavigationLink {
+                OnboardingCompletedView()
+            } label: {
+                Text("Get Started")
+                    .callToActionButton()
+                
+            }
+            
+            Text("Already have an account? Sign in!")
+                .underline()
+                .font(.body)
+                .padding(8)
+                .tappableBackground()
+                .onTapGesture {
                     
                 }
+        }
+    }
+    
+    private var policyLinks: some View {
+        HStack(spacing: 8) {
+            Link(destination: URL(string: Constants.termsOfServiceUrl)!) {
+                Text("Terms of Service")
             }
-            .padding(16)
+            Circle()
+                .fill(.accent)
+                .frame(width: 4, height: 4)
+            Link(destination: URL(string: Constants.privacyPolicyUrl)!) {
+                Text("Terms of Service")
+            }
         }
     }
 }
