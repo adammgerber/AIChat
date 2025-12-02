c4d77a2566156a0601a3c929cf46e4cb638eba39
adammgerber <agerber7777@gmail.com>
2025-12-02 11:14:34 +0200

    [Feature] Add app icon, launch screen

---
+++ AIChatCourse/AIChatCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatCourseApp.swift
+//  AIChatCourse
+//
+//  Created by Adam Gerber on 09/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct AIChatCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+           ContentView()
+        }
+    }
+}
