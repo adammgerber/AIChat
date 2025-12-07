b7a6605315835606a93cf54a93948ccfb9403374
adammgerber <agerber7777@gmail.com>
2025-12-07 12:56:08 +0200

    [Feature CarouselView

---
+++ AIChatCourse/Components/Views/CarouselView.swift
@@ -0,0 +1,70 @@
+//
+//  CarouselViewswift.swift
+//  AIChatCourse
+//
+//  Created by Adam Gerber on 07/12/2025.
+//
+
+import SwiftUI
+
+struct CarouselView<Content: View, T: Hashable>: View {
+    
+    var items: [T]
+    @ViewBuilder var content: (T) -> Content
+    @State private var selection: T?
+    
+    var body: some View {
+        VStack {
+            ScrollView(.horizontal) {
+                LazyHStack(spacing: 0) {
+                    ForEach(items, id: \.self) { item in
+                        content(item)
+                        .scrollTransition(.interactive.threshold(.visible(0.95)), transition: { content, phase in
+                            content
+                                .scaleEffect(phase.isIdentity ? 1 : 0.9)
+                        })
+                        .containerRelativeFrame(.horizontal, alignment: .center)
+                        .id(item)
+                    }
+                }
+            }
+            .frame(height: 200)
+            .scrollIndicators(.hidden)
+            .scrollTargetLayout()
+            .scrollTargetBehavior(.paging)
+            .scrollPosition(id: $selection)
+            .onChange(of: items.count, {_, _ in
+                updateSelectionIfNeeded()
+            })
+            .onAppear {
+                updateSelectionIfNeeded()
+            }
+            
+            HStack(spacing: 8) {
+                ForEach(items, id: \.self) { item in
+                    Circle()
+                        .fill(item == selection ? .accent : .secondary)
+                        .frame(width: 8)
+                }
+            }
+            .animation(.linear, value: selection)
+        }
+    }
+    
+    private func updateSelectionIfNeeded() {
+        if selection == nil {
+            selection = items.first
+        }
+    }
+}
+
+#Preview {
+    CarouselView(items: AvatarModel.mocks) { item in
+        HeroCellView(
+            title: item.name,
+            subtitle: item.characterDescription,
+            imageName: item.profileImageName
+        )
+    }
+    .padding()
+}
