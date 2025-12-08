bf57c7962fccdcfcf5ede97838fd5673b8344739
adammgerber <agerber7777@gmail.com>
2025-12-08 11:02:00 +0200

    [Feature] CustomListCellView

---
+++ AIChatCourse/Components/Views/CustomListCellView.swift
@@ -0,0 +1,58 @@
+//
+//  CustomListCellView.swift
+//  AIChatCourse
+//
+//  Created by Adam Gerber on 08/12/2025.
+//
+
+import SwiftUI
+
+struct CustomListCellView: View {
+    
+    var imageName: String? = Constants.randomImage
+    var title: String? = "Alpha"
+    var subtitle: String? = "An alient that is smiling in the park."
+    
+    var body: some View {
+        HStack(spacing: 8) {
+            ZStack {
+                if let imageName {
+                    ImageLoaderView(urlString: imageName)
+                } else {
+                    Rectangle()
+                        .fill(.secondary.opacity(0.5))
+                }
+            }
+            .aspectRatio(1, contentMode: .fit)
+            .frame(height: 60)
+            .cornerRadius(16)
+            
+            VStack(alignment: .leading, spacing: 4) {
+                if let title {
+                    Text(title)
+                        .font(.headline)
+                }
+                if let subtitle {
+                    Text(subtitle)
+                        .font(.subheadline)
+                }
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        .padding(12)
+        .padding(.vertical, 4)
+        .background(Color(uiColor: .systemBackground))
+    }
+}
+
+#Preview {
+    ZStack {
+        Color.gray.opacity(0.2).ignoresSafeArea()
+        VStack {
+            CustomListCellView()
+            CustomListCellView(title: nil)
+            CustomListCellView(subtitle: nil)
+            CustomListCellView(imageName: nil)
+        }
+    }
+}
