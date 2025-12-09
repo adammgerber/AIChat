2c9a0e05732f1a538beb7dea4aaed7d2a7f268e4
adammgerber <agerber7777@gmail.com>
2025-12-09 16:37:50 +0200

    [Feature] ChatBubbleView

---
+++ AIChatCourse/Components/Views/ChatBubbleView/ChatBubbleView.swift
@@ -0,0 +1,70 @@
+//
+//  ChatBubbleView.swift
+//  AIChatCourse
+//
+//  Created by Adam Gerber on 09/12/2025.
+//
+
+import SwiftUI
+
+struct ChatBubbleView: View {
+    
+    
+    var text: String = "This is sample text"
+    var textColor: Color = .primary
+    var backgroundColor: Color = .gray
+    var showImage: Bool = true
+    var imageName: String?
+    
+    let offset: CGFloat = 14
+    
+    var body: some View {
+        HStack(alignment: .top, spacing: 8) {
+            if showImage {
+                ZStack {
+                    if let imageName {
+                        ImageLoaderView(urlString: imageName)
+                    } else {
+                        Rectangle()
+                            .fill(.secondary)
+                    }
+                }
+                .frame(width: 45, height: 45)
+                .clipShape(Circle())
+                .offset(y: offset)
+            }
+            
+            Text(text)
+                .font(.body)
+                .foregroundStyle(textColor)
+                .padding(.horizontal, 16)
+                .padding(.vertical, 10)
+                .background(backgroundColor)
+                .cornerRadius(6)
+        }
+        .padding(.bottom, showImage ? offset : 0)
+    }
+}
+
+#Preview {
+    ScrollView {
+        VStack(spacing: 16) {
+            ChatBubbleView()
+            ChatBubbleView(text: "Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.")
+            ChatBubbleView(
+                textColor: .white,
+                backgroundColor: .accent,
+                showImage: false,
+                imageName: nil
+            )
+            ChatBubbleView(
+                text: "Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.",
+                textColor: .white,
+                backgroundColor: .accent,
+                showImage: false,
+                imageName: nil
+            )
+        }
+    }
+    .padding(8)
+}
---
+++ AIChatCourse/Components/Views/ChatBubbleView/ChatBubbleViewBuilder.swift
@@ -0,0 +1,58 @@
+//
+//  ChatBubbleViewBuilder.swift
+//  AIChatCourse
+//
+//  Created by Adam Gerber on 09/12/2025.
+//
+
+import SwiftUI
+
+struct ChatBubbleViewBuilder: View {
+    var message: ChatMessageModel = .mock
+    var isCurrentUser: Bool = false
+    var imageName: String?
+    
+    var body: some View {
+        ChatBubbleView(
+            text: message.content ?? "",
+            textColor: isCurrentUser ? .white : .primary,
+            backgroundColor: isCurrentUser ? .accent : Color(uiColor: .systemGray6),
+            showImage: !isCurrentUser,
+            imageName: imageName
+        )
+        .frame(maxWidth: .infinity, alignment: isCurrentUser ? .trailing : .leading)
+        .padding(.leading, isCurrentUser ? 75 : 0)
+        .padding(.trailing, isCurrentUser ? 0 : 90)
+    }
+}
+
+#Preview {
+    ScrollView {
+        VStack(spacing: 24) {
+            ChatBubbleViewBuilder()
+            ChatBubbleViewBuilder(isCurrentUser: true)
+            ChatBubbleViewBuilder(
+                message: ChatMessageModel(
+                    id: UUID().uuidString,
+                    chatId: UUID().uuidString,
+                    authorId: UUID().uuidString,
+                    content: "This is some longer content that goes on multiple lines tha tgoies another line!",
+                    seenByIds: nil,
+                    dateCreated: .now
+                )
+            )
+            ChatBubbleViewBuilder(
+                message: ChatMessageModel(
+                    id: UUID().uuidString,
+                    chatId: UUID().uuidString,
+                    authorId: UUID().uuidString,
+                    content: "This is some longer content that goes on multiple lines tha tgoies another line!",
+                    seenByIds: nil,
+                    dateCreated: .now
+                ),
+                isCurrentUser: true
+            )
+        }
+        .padding(12)
+    }
+}
