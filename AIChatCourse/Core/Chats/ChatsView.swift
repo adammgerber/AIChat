b520f59872655f60f63fea6d3df2421f67f65d77
adammgerber <agerber7777@gmail.com>
2025-12-18 15:27:46 +0200

    [Feature] RecentAvatars

--- AIChatCourse/Core/Chats/ChatsView.swift
+++ AIChatCourse/Core/Chats/ChatsView.swift
@@ -10,10 +10,71 @@ import SwiftUI
 struct ChatsView: View {
     @State private var chats: [ChatModel] = ChatModel.mocks
     @State private var path: [NavigationPathOption] = []
+    @State private var recentAvatars: [AvatarModel] = AvatarModel.mocks
     
     var body: some View {
         NavigationStack(path: $path) {
             List {
+                if !recentAvatars.isEmpty {
+                    recentsSection
+                }
+                
+                chatsSection
+            }
+            .navigationTitle("chats")
+            .navigationDestinationForCoreModule(path: $path)
+        }
+    }
+    
+    private func onAvatarPressed(avatar: AvatarModel) {
+        path.append(.chat(avatarId: avatar.avatarId))
+    }
+    
+    private func onChatPressed(chat: ChatModel) {
+        path.append(.chat(avatarId: chat.avatarId))
+    }
+    
+    private var recentsSection: some View {
+        Section {
+            ScrollView(.horizontal) {
+                LazyHStack(spacing: 8) {
+                    ForEach(recentAvatars, id: \.self) { avatar in
+                        if let imageName = avatar.profileImageName {
+                            VStack(spacing: 8) {
+                                ImageLoaderView(urlString: imageName)
+                                    .aspectRatio(1, contentMode: .fit)
+                                    .clipShape(Circle())
+                                
+                                Text(avatar.name ?? "")
+                                    .font(.caption)
+                                    .foregroundStyle(.secondary)
+                            }
+                            .anyButton {
+                                onAvatarPressed(avatar: avatar)
+                            }
+                        }
+                    }
+                }
+                .padding(.top, 12)
+            }
+            .frame(height: 120)
+            .removeListRowFormatting()
+        } header: {
+            Text("Recents")
+        }
+    }
+    
+    private var chatsSection: some View {
+        Section {
+            if chats.isEmpty {
+                Text("Your chats will appear here!")
+                    .foregroundStyle(.secondary)
+                    .font(.title3)
+                    .frame(maxWidth: .infinity)
+                    .multilineTextAlignment(.center)
+                    .padding(40)
+                    .removeListRowFormatting()
+            } else {
                 ForEach(chats) { chat in
                     ChatRowCellViewBuilder(
                         currentUserId: nil, // Add cuid
@@ -33,13 +94,9 @@ struct ChatsView: View {
                     .removeListRowFormatting()
                 }
             }
-            .navigationTitle("chats")
-            .navigationDestinationForCoreModule(path: $path)
+        } header: {
+            Text("Chats")
         }
-    }
-    
-    private func onChatPressed(chat: ChatModel) {
-        path.append(.chat(avatarId: chat.avatarId))
     }
 }
 
